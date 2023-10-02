9f6dc88c0d93655a1f1392be4c84cb3ef1f4fa4d
TJ Awojana <tj.tolulope@dominodatalab.com>
2023-10-02 09:07:32 -0700

    Pushed from Domino: https://prod-field.cs.domino.tech/workspace/tj_tolulope/tj-dca?executionId=651ae96ef791801686615a48

---
+++ app.py
@@ -0,0 +1,23 @@
+import flask
+from flask import request, redirect, url_for
+
+class ReverseProxied(object):
+  def __init__(self, app):
+      self.app = app
+  def __call__(self, environ, start_response):
+      script_name = environ.get('HTTP_X_SCRIPT_NAME', '')
+      if script_name:
+          environ['SCRIPT_NAME'] = script_name
+          path_info = environ['PATH_INFO']
+          if path_info.startswith(script_name):
+              environ['PATH_INFO'] = path_info[len(script_name):]
+      return self.app(environ, start_response)
+
+app = flask.Flask(__name__)
+app.wsgi_app = ReverseProxied(app.wsgi_app)
+
+# Homepage which uses a template file
+@app.route('/')
+def index_page():
+  return flask.render_template("index.html")
+
